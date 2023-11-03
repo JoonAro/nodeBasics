6edda259fd84204783891e5a51dfec4a8d17a64b
joonaro <cupetus@gmail.com>
2023-11-03 15:17:23 +0200

    changed h1 style

--- week1/carServer.js
+++ week1/carServer.js
@@ -2,31 +2,34 @@
 
 const http = require('http');
 
-const {port,host} = require('./config.json');
+const { port, host } = require('./config.json');
 
 const cars = require('./cars.json');
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    res.end(createHtml(cars));
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' });
+  res.end(createHtml(cars));
 });
 
-server.listen(port,host,()=>console.log(`${host}:${port}...`));
+server.listen(port, host, () => console.log(`${host}:${port}...`));
 
-function createHtml(carArray){
-    let htmlString=`<!DOCTYPE html>
+function createHtml(carArray) {
+  let htmlString = `<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
         <title>Cars</title>
       </head>
+      <style>
+      h1{"color:yellow"}
+      </style>
       <body>
-        <h1 style="color:yellow">Cars</h1>`;
+        <h1>Cars</h1>`;
 
-    for(const car of carArray){
-        htmlString+=`<h2 style="color:${car.color}">${car.model}:${car.licence}</h2>\n`;
-    }
-    htmlString +=`</body>
+  for (const car of carArray) {
+    htmlString += `<h2 style="color:${car.color}">${car.model}:${car.licence}</h2>\n`;
+  }
+  htmlString += `</body>
                 </html>`;
-    return htmlString;
+  return htmlString;
 }
