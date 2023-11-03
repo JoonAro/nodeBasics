8327321e99f909243bff61973883cef7d8243609
joonaro <cupetus@gmail.com>
2023-11-03 15:14:44 +0200

    first init

---
+++ week2/htmlFromFileB.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs').promises;
+
+const { port, host } = require(path.join(__dirname, 'config.json'));
+//absolute path to folder where htmlFromFile.js is __dirname
+const homePath = path.join(__dirname, 'home.html');
+
+const server = http.createServer((req, res) => {
+    sendFile(res, homePath);
+});
+
+server.listen(port, host,
+    () => console.log(`${host}:${port} serving...`));
+
+async function sendFile(res, filepath) {
+    try {
+        const data = await fs.readFile(homePath, 'utf8');
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'content-Length': Buffer.byteLength(data, 'utf8')
+        });
+        res.end(data);
+    }
+    catch (err) {
+        res.statusCode = 404;
+        res.end('Error: ${err.message}');
+    }
+}
